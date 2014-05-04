cae611140edbbc6aa531f51360d4f4a601b7ef3d
Chilledheart <rwindz0@gmail.com>
2014-05-04 10:46:12 +0800

    git-s alias added

--- gitconfig_setup.sh
+++ gitconfig_setup.sh
@@ -12,6 +12,7 @@ git config --global alias.ci commit
 git config --global alias.br branch
 git config --global alias.d difftool
 git config --global alias.h help
+git config --global alias.s "status -s -b"
 git config --global alias.sub submodule
 git config --global alias.unstage "reset HEAD"
 git config --global alias.unstash "stash pop"
