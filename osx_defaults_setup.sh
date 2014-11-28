401e683c6dbdad7739d98573b6f87d044a11c937
Chilledheart <rwindz0@gmail.com>
2014-11-28 17:47:48 +0800

    update osx defaults

--- osx_defaults_setup.sh
+++ osx_defaults_setup.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-# Borrowed from 
+# this part comes from
 # https://github.com/holman/dotfiles/blob/master/osx/set-defaults.sh
 # Set a really fast key repeat.
 defaults write NSGlobalDomain KeyRepeat -int 0
@@ -15,4 +15,27 @@ defaults write com.apple.Finder FXPreferredViewStyle Nlsv
 defaults write com.apple.finder AppleShowAllFiles -bool true
 # Finder: show all filename extensions
 #defaults write NSGlobalDomain AppleShowAllExtensions -bool true
+
+# this part comes from
+# https://github.com/mathiasbynens/dotfiles/blob/master/.osx
+# Increase window resize speed for Cocoa applications
+defaults write NSGlobalDomain NSWindowResizeTime -float 0.001
+
+# Disable the crash reporter
+defaults write com.apple.CrashReporter DialogType -string "none"
+
+# Restart automatically if the computer freezes
+sudo systemsetup -setrestartfreeze on
+
+# Disable local Time Machine snapshots
+sudo tmutil disablelocal
+
+# Disable hibernation (speeds up entering sleep mode)
+sudo pmset -a hibernatemode 0
+# Remove the sleep image file to save disk space
+sudo rm /private/var/vm/sleepimage
+# Create a zero-byte file instead…
+sudo touch /private/var/vm/sleepimage
+# …and make sure it can’t be rewritten
+sudo chflags uchg /private/var/vm/sleepimage
 
