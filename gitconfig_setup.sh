147022865d789068e0e18a6606bdd9fc2226352a
Chilledheart <rwindz0@gmail.com>
2015-03-06 23:54:58 +0800

    git: add wshow alias

--- gitconfig_setup.sh
+++ gitconfig_setup.sh
@@ -17,6 +17,7 @@ git config --global alias.unstage "reset HEAD"
 git config --global alias.unstash "stash pop"
 git config --global alias.dst "diff --staged"
 git config --global alias.cp "cherry-pick"
+git config --global alias.wshow "show --color-words"
 git config --global alias.wdiff "diff --color-words"
 
 git config --global alias.lg "log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit --date=relative"
